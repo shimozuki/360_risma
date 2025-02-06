af106b7936fbcb47627545d7ad40f4ca98e2865f
robbishimozuki <robbishimozuki@gmail.com>
2025-02-06 20:39:50 +0800

    master gun

--- lib/panorama.dart
+++ lib/panorama.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:panorama/panorama.dart';
 
-class PanoramaPage extends StatelessWidget {
+class PanoramaPage extends StatefulWidget {
+  @override
+  _PanoramaPageState createState() => _PanoramaPageState();
+}
+
+class _PanoramaPageState extends State<PanoramaPage> {
+  final List<String> images = [
+    'assets/scene_1.jpg',
+    'assets/scene_2.jpg',
+    'assets/scene_3.jpg',
+    'assets/scene_4.jpg',
+    'assets/scene_5.jpg',
+  ];
+
+  int currentIndex = 0;
+
+  void _onTap() {
+    setState(() {
+      currentIndex = (currentIndex + 1) %
+          images.length; // Gunakan modulo untuk mengulang indeks
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Foto 360 Derajat'),
-      ),
-      body: Panorama(
-        child: Image.asset('assets/PANO_20250131_081854~2.jpg'),
+    return GestureDetector(
+      onTap: _onTap,
+      child: Panorama(
+        child: Image.asset(images[currentIndex]),
       ),
     );
   }
